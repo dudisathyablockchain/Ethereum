2fcc5e2db88436d78aa5938e7126121050b6fa47
dudisathyablockchain <39380463+dudisathyablockchain@users.noreply.github.com>
2020-02-26 12:19:00 +0530

    made changes to compile latest compiler versions

--- Usecases/Inbox.sol
+++ Usecases/Inbox.sol
@@ -1,18 +1,18 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.4.22 <0.6.0;
 
 contract Inbox {
     
     string public message;
     
-    constructor(string initialMessage) public {
+    Inbox(string initialMessage) public {
         message = initialMessage;
     }
     
-    function setMessage(string newMessage) public {
+    function setMessage(string  memory newMessage) public {
         message = newMessage;
     }
     
-    function getMessage() public view returns (string) {
+    function getMessage() public view returns (string memory) {
         return message;
     }
     
